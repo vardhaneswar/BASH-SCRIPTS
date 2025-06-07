db386d239bf3718bb63d71e58dab467af7de27ab
eswar <vardhaneswar213@gmail.com>
2025-06-07 12:55:08 -0400

    simple bash script

---
+++ BASICS.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#############
+#Author: Eswar Vardhan
+#Date: 2023-10-01
+#Description: This script is a collection of basic bash commands and their explanations.
+#############
+
+#simple hello world script
+echo "Hello, World!"
+# This is a comment in bash
